9174178ba8f441a7f1b48cfb4d5645a88764ad92
Ben Murrell <murrellb@gmail.com>
2023-09-24 00:15:11 +0200

    Updating fit script

--- src/fitMLG.jl
+++ src/fitMLG.jl
@@ -1,12 +1,13 @@
+#This can all be run from the REPL
 using CSV, DataFrames, StatsBase, Statistics, Dates, Flux
 
 #If you're running this on an NVIDIA GPU:
-#using CUDA
-#device = gpu
+using CUDA
+device = gpu
 ########################################
 
 #If no NVIDIA GPU#######################
-device = cpu
+#device = cpu
 ########################################
 
 count_path = "data/2023-09-09_tall_counts.csv"
@@ -24,64 +25,79 @@ dateinds = indmap(dates)
 #We'll need numerical "days" for the LF calculations too - We'll make day zero the most recent date
 days = [d.value for d in dates .- maximum(dates)]
 
-#lineages by countries by days
+#lineages-by-countries-by-days
 count_tensor = zeros(Int64,length(lineages), length(countries), length(dates))
 d = count_df
 for r in 1:size(d,1)
     count_tensor[lineageinds[d.lineage[r]],countryinds[d.country[r]],dateinds[d.date[r]]] = d.count[r]
 end
 
+#Setting up model using Flux
 struct MLR
   R
   C
 end
 Flux.@functor MLR
 MLR(T, lineages::Integer, countries::Integer) = MLR(zeros(T,lineages), zeros(T,lineages,countries,1))
+#Scaling of R makes rates interpretable in years, scaling of C to help model convergence
+(m::MLR)(days) = Flux.unsqueeze((m.R ./ 365.0f0)*days', dims = 2) .+ m.C .* 10.0f0
 
-#Scaling of R makes rates interpretable in years
-#scaling of C to help model convergence
-(m::MLR)(days) = Flux.unsqueeze((m.R ./ 365.0f0)*days', dims = 2) .+ m.C
-
+#Init model, and get it onto the GPU
 model = MLR(Float32,length(lineages),length(countries)) |> device
 days, count_tensor = (days, count_tensor) |> device
 
-#Start learning rate on 0.1f0, run until its oscilating, then set to 0.01f0 and continue
+#Fitting model
 opt = Flux.Optimiser(Flux.WeightDecay(1.0f-10), AdamW(0.1f0))
 ps = Flux.params(model)
 
-for i in 1:10000
+current_l = Inf
+@time for i in 1:100000 #323.014108 seconds
     l,gs = Flux.withgradient(ps) do
             -mean(logsoftmax(model(days)) .* count_tensor)
         end
     Flux.Optimise.update!(opt, ps, gs)
-    if i % 10 == 0
-         print(l, ": ")
+    #Everything below here is monitoring the fit, adjusting the learning rate, and stopping
+    if i % 100 == 0
+        model.R .= model.R .- mean(model.R) #Centering R, removing an identifiability issue
+        print(l, "; ", opt[2][1].eta, "; ")
         #Monitoring growth of 3 lineages spread over the expected range of R
         println([model.R[lineages .== "BA.2"][1], model.R[lineages .== "XBB.1.5"][1], model.R[lineages .== "HK.3"][1]])
+        if l > current_l
+            opt[2][1].eta = opt[2][1].eta * 0.85f0
+            println("LR decay")
+            if opt[2][1].eta < 0.0001f0 #Stopping criterion
+                println("Done")
+                break
+            end
+        else
+            current_l = l
+        end
     end
 end
-#To do: stopping condition, and LR decay
+#Should look something like:
+#0.1746748; 0.00010854139479114348; Float32[-71.824394, 27.58488, 85.06291]
 
 #Saving model fit
 using BSON
+mkpath("model_fits")
 cpumodel = cpu(model) #BSON doesn't like GPU arrays
-@BSON.save "modelfit.bson" cpumodel
+@BSON.save "model_fits/modelfit.bson" cpumodel
 
 #Saving inferred rates
 rate_df = DataFrame()
 rate_df.pango = lineages
 rate_df.R = Float64.(model.R)
 rate_df.seq_volume = sum(count_tensor,dims = (2,3))[:]
-CSV.write("Rates.csv",rate_df)
+CSV.write("model_fits/rates.csv",rate_df)
 for c in 1:length(countries)
     rate_df[!,"seq_volume_$(countries[c])"] = sum(count_tensor[:,c,:],dims = 2)[:]
 end
-CSV.write("Rates_with_country_volumes.csv",rate_df)
-
+CSV.write("model_fits/rates_with_country_volumes.csv",rate_df)
 
 #Plotting fit to countries
 using Plots
 plotpath = "plots"
+mkpath(plotpath)
 model, days, count_tensor = (model, days, count_tensor) |> cpu
 mod_fit = softmax(model(days), dims = 1)
 freq_tensor = count_tensor ./ sum(count_tensor, dims = 1)
@@ -108,4 +124,3 @@ for country_to_plot in 1:length(countries)
     end
     savefig(pl, joinpath(plotpath,"$(countries[country_to_plot]).pdf"))
 end
-
